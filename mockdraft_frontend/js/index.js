972e4fd0d74165a4edaaefe21b3f7766b9b08bb9
ellisandrews <ellisandrews1@gmail.com>
2020-04-14 15:24:08 -0500

    Add draft player JS functionality

--- mockdraft_frontend/js/index.js
+++ mockdraft_frontend/js/index.js
@@ -125,99 +125,93 @@ const findOpenRosterPositions = roster => {
     return rosterPositions.filter(position => !takenPositions.includes(position));
 }
 
-const determineRosterPosition = async (position, rosterId) => {
+const determineRosterPosition = (position, rosterId) => {
     // Figure out which roster position to put the player in given their position
-
-    // Fetch the roster from the backend
-    const roster = await fetchRoster(rosterId)
-
-    // Find the open positions on the roster (an array)
-    const openPositions = findOpenRosterPositions(roster)
-    const firstBench = openPositions.find(position => position.startsWith('BENCH'))
-
-    // Select one of the open positions based on some logic based on the position
-    switch (position) {
-        case 'QB':
-            if (openPositions.includes('QB')) {
-                return 'QB'
-            } else {
-                return firstBench
-            }
-        case 'RB':
-            if (openPositions.includes('RB1')) {
-                return 'RB1'
-            } else if (openPositions.includes('RB2')) {
-                return 'RB2'
-            } else if (openPositions.includes('FLEX')) {
-                return 'FLEX'
-            } else {
-                return firstBench
-            }
-        case 'WR':
-            if (openPositions.includes('WR1')) {
-                return 'WR1'
-            } else if (openPositions.includes('WR2')) {
-                return 'WR2'
-            } else if (openPositions.includes('FLEX')) {
-                return 'FLEX'
-            } else {
-                return firstBench
-            }
-        case 'TE':
-            if (openPositions.includes('TE')) {
-                return 'TE'
-            } else if (openPositions.includes('FLEX')) {
-                return 'FLEX'
-            } else {
-                return firstBench
-            }
-        case 'DST':
-            if (openPositions.includes('DST')) {
-                return 'DST'
-            } else {
-                return firstBench
-            }
-        case 'K':
-            if (openPositions.includes('K')) {
-                return 'K'
-            } else {
-                return firstBench
-            }
-    }
-}
-
-
-const draftPlayer = async (playerId, rosterId) => {
     
-    // Fetch the player object
-    const player = await fetchPlayer(playerId)
+    return fetchRoster(rosterId)
+        .then(roster => {
+            // Find the open positions on the roster (an array)
+            const openPositions = findOpenRosterPositions(roster)
+            
+            // Find the first open BENCH position
+            const firstBench = openPositions.find(position => position.startsWith('BENCH'))
 
-    // Determine the roster position for the player to fill
-    const rosterPosition = await determineRosterPosition(player.position, rosterId)
-
-    console.log(rosterPosition)
-
-    // // Make a request to add the player to the roster in a certain position
-    // reqObj = {
-    //     method: 'PATCH',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //         roster_id: rosterId,
-    //         roster_position: rosterPosition
-    //     })
-    // }
-
-    // // Update the backend, then conditionally update the frontend
-    // fetch(`${APIBASE}/players/${playerId}`, reqObj)
-    //     .then(parseJSONResponse)
-    //     .then(player => {
-    //         console.log(player)
-    //     })
-    //     .catch(logError)
+            // Select one of the open positions based on some logic based on the position
+            switch (position) {
+                case 'QB':
+                    if (openPositions.includes('QB')) {
+                        return 'QB'
+                    } else {
+                        return firstBench
+                    }
+                case 'RB':
+                    if (openPositions.includes('RB1')) {
+                        return 'RB1'
+                    } else if (openPositions.includes('RB2')) {
+                        return 'RB2'
+                    } else if (openPositions.includes('FLEX')) {
+                        return 'FLEX'
+                    } else {
+                        return firstBench
+                    }
+                case 'WR':
+                    if (openPositions.includes('WR1')) {
+                        return 'WR1'
+                    } else if (openPositions.includes('WR2')) {
+                        return 'WR2'
+                    } else if (openPositions.includes('FLEX')) {
+                        return 'FLEX'
+                    } else {
+                        return firstBench
+                    }
+                case 'TE':
+                    if (openPositions.includes('TE')) {
+                        return 'TE'
+                    } else if (openPositions.includes('FLEX')) {
+                        return 'FLEX'
+                    } else {
+                        return firstBench
+                    }
+                case 'DST':
+                    if (openPositions.includes('DST')) {
+                        return 'DST'
+                    } else {
+                        return firstBench
+                    }
+                case 'K':
+                    if (openPositions.includes('K')) {
+                        return 'K'
+                    } else {
+                        return firstBench
+                    }
+            }
+        })
 }
 
+const addPlayerToRoster = (playerId, rosterId, rosterPosition) => {
+    // Make a request to add the player to the roster in a certain position
+    reqObj = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            roster_id: rosterId,
+            roster_position: rosterPosition
+        })
+    }
+
+    // Update the backend
+    return fetch(`${APIBASE}/players/${playerId}`, reqObj).then(parseJSONResponse)
+}
+
+const draftPlayer = (playerId, rosterId) => {
+    // Draft a player to a roster (assigning the roster_position appropriately)
+    fetchPlayer(playerId)
+        .then(player => determineRosterPosition(player.position, rosterId))
+        .then(rosterPosition => addPlayerToRoster(playerId, rosterId, rosterPosition))
+        .then(fetchAndDisplayRoster(rosterId))
+}
 
 const displayRoster = roster => {
 
