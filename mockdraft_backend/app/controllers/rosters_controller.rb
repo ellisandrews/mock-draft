a0c71b7c4de557ae95ef1d9077b697eb00e97680
ellisandrews <ellisandrews1@gmail.com>
2020-04-15 11:43:01 -0500

    Initial work on backend API

--- mockdraft_backend/app/controllers/rosters_controller.rb
+++ mockdraft_backend/app/controllers/rosters_controller.rb
@@ -1,7 +1,21 @@
 class RostersController < ApplicationController
 
     def index
-        @rosters = RostersController.all
+        rosters = Roster.all
+        render json: rosters.map { |roster| RosterSerializer.new(roster).to_serialized_hash }
     end
-    
-def
+
+    def show
+        roster = Roster.find(params[:id])
+        render json: RosterSerializer.new(roster).to_serialized_json
+    end
+
+    def create
+        
+    end
+
+    def update
+    end
+
+end
+
