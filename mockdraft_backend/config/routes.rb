53b06c6b7a1f31bb58c0bbe46e328bf983797aca
ellisandrews <ellisandrews1@gmail.com>
2020-04-15 12:57:04 -0500

    Build out queue items controller

--- mockdraft_backend/config/routes.rb
+++ mockdraft_backend/config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :owners, only: [:index, :show, :create]
   resources :players, only: [:index, :show, :update]
   resources :rosters, only: [:index, :show, :create, :update]
-  
-  # resources :queues
-  # resources :playerqueues
+  resources :queue_items, only: [:create, :destroy]
+
 end
